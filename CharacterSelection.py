d8b468a77a7123e76e9792f7e21efb89c4272bd9
td2thinh <75013885+td2thinh@users.noreply.github.com>
2020-12-04 10:37:45 +0700

    The main game
    this includes: Map randomized, Character sets randomized, Character Selection, Start racing, Result Screen.

---
+++ CharacterSelection.py
@@ -0,0 +1,49 @@
+from ImageAssets import *
+from StartGame import *
+
+# Write code to get player name from here
+playerName = 'Noobmaster69'
+
+
+def TraceCharacterSelection(x, y):
+    for i in range(6):
+        CharList[i].speed(0)
+        CharList[i].hideturtle()
+        CharList[i].penup()
+        CharList[i].shape(avatarsChose[i])
+        CharList[i].goto(x, y)
+        CharList[i].showturtle()
+        x = x + 100
+
+# This whole block of code is used to assign playerName to Character's name, not efficient but it works
+# Once Character is selected it starts the game
+
+
+def PlayerAssign0(x, y):
+    NameList[0] = playerName
+    StartGame()
+
+
+def PlayerAssign1(x, y):
+    NameList[1] = playerName
+    StartGame()
+
+
+def PlayerAssign2(x, y):
+    NameList[2] = playerName
+    StartGame()
+
+
+def PlayerAssign3(x, y):
+    NameList[3] = playerName
+    StartGame()
+
+
+def PlayerAssign4(x, y):
+    NameList[4] = playerName
+    StartGame()
+
+
+def PlayerAssign5(x, y):
+    NameList[5] = playerName
+    StartGame()
