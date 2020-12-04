fab106d54b8c9c0d2ca770671bfb1c877fb4b31b
td2thinh <75013885+td2thinh@users.noreply.github.com>
2020-12-04 15:40:18 +0700

    included the main game

--- PlayGame.py
+++ PlayGame.py
@@ -4,19 +4,20 @@ import time
 from ResultScreen import *
 from CharacterSelection import *
 from ImageAssets import *
-from StartGame import *
+from StartRacing import *
 # This file is used to play the CharacterSelection Screen
 
-screen.setup(1200, 645)
 
+def PlayGame(x, y):
+    screen.setup(1200, 645)
 
-TraceCharacterSelection(-250, 0)
+    TraceCharacterSelection(-250, 0)
 
-CharList[0].onclick(PlayerAssign0)
-CharList[1].onclick(PlayerAssign1)
-CharList[2].onclick(PlayerAssign2)
-CharList[3].onclick(PlayerAssign3)
-CharList[4].onclick(PlayerAssign4)
-CharList[5].onclick(PlayerAssign5)
+    CharList[0].onclick(PlayerAssign0)
+    CharList[1].onclick(PlayerAssign1)
+    CharList[2].onclick(PlayerAssign2)
+    CharList[3].onclick(PlayerAssign3)
+    CharList[4].onclick(PlayerAssign4)
+    CharList[5].onclick(PlayerAssign5)
 
-input()
+    input()
